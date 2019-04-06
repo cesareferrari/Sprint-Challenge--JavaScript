393839f868bf11d1da4a98c89b9b42e445d83b8a
Cesare Ferrari <cesare@maryandferrari.com>
2019-04-06 12:47:38 -0400

    Find total animal population

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -180,7 +180,7 @@ console.log(largerPopulation);
 The zoos need to know their total animal population across the United States.  Find the total population from all the zoos using the .reduce() method.
 
 */
-const populationTotal = 0;
+const populationTotal = zooAnimals.reduce((sum, animal) => { return sum + animal.population }, 0);
 console.log(populationTotal);
 
 
